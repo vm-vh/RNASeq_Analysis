a24178047bdf6ddbf1c31a4aaa0fdcd145b5e1c8
Vincent M. von Häfen <113845446+vm-vh@users.noreply.github.com>
2023-04-13 10:12:58 +0200

    Update kallisto_index.sh

--- kallisto_index.sh
+++ kallisto_index.sh
@@ -1,26 +1,20 @@
 # !/bin/bash
-# This script checks the qualitiy of our fastq files and performs an alignment to the tomato cDNA transcriptome reference with Kallisto.
-# To run this 'shell script' you will need to open your terminal and navigate to the directory where this script resides on your computer.
-# This should be the same directory where you fastq files and reference fasta file are found.
-# Change permissions on your computer so that you can run a shell script by typing: 'chmod 777 readMapping.sh' (without the quotes) at the terminal prompt 
-# Then type './readMapping.sh' (without the quotes) at the prompt.  
-# This will begin the process of running each line of code in the shell script.
+# This script checks the qualitiy of the fastq files and performs an alignment to the cDNA transcriptome reference with Kallisto.
 
-# first use fastqc to check the quality of our fastq files:
-# fastqc *.gz -t 4
-
-# next, we want to build an index from our reference fasta file 
-# I get my reference plant transcriptome files from here: https://plants.ensembl.org/info/data/ftp/index.html
+# build an index from the reference fasta file 
+# reference plant transcriptome files: https://plants.ensembl.org/Coffea_canephora/Info/Index.html
 kallisto index -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.fa
 
-# now map reads to the indexed reference host transcriptome
-# use as many 'threads' as your machine will allow in order to speed up the read mapping process.
-# note that we're also including the '&>' at the end of each line
-# this takes the information that would've been printed to our terminal, and outputs this in a log file that is saved in /data/course_data
-
-kallisto quant -i Solanum_lycopersicum.SL3.0.cdna.all.index -o k_SRR17655898  --single -l 180 -s 20 SRR17655898.trim.fastq.gz
-
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR --single -l 180 -s 20 SRR.trim.fastq.gz
+# individual leaves
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673027 --single -l 180 -s 20 SRR17673027.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673026 --single -l 180 -s 20 SRR17673026.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673025 --single -l 180 -s 20 SRR17673025.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673024 --single -l 180 -s 20 SRR17673024.trim.fastq.gz
+# individual stems
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673033 --single -l 180 -s 20 SRR17673033.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673032 --single -l 180 -s 20 SRR17673032.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673031 --single -l 180 -s 20 SRR17673031.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673030 --single -l 180 -s 20 SRR17673030.trim.fastq.gz
 
 # summarize fastqc and kallisto mapping results in a single summary html using MultiQC
 # multiqc -d . 
