d492d80743204bd277aeb53e3978ae5be77e64fd
vm-vh <vincentvonhaefen@gmail.com>
2023-04-15 03:49:56 +0200

    added kallisto output and log

--- kallisto.sh
+++ kallisto.sh
@@ -2,19 +2,19 @@
 # This script checks the qualitiy of the fastq files and performs an alignment to the cDNA transcriptome reference with Kallisto.
 
 # build an index from the reference fasta file 
-# reference plant transcriptome files: https://plants.ensembl.org/Coffea_canephora/Info/Index.html
+# reference plant transcriptome file: https://plants.ensembl.org/Coffea_canephora/Info/Index.html
 kallisto index -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.fa
 
-# individual leaves
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673027 --single -l 180 -s 20 SRR17673027.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673026 --single -l 180 -s 20 SRR17673026.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673025 --single -l 180 -s 20 SRR17673025.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673024 --single -l 180 -s 20 SRR17673024.trim.fastq.gz
 # individual stems
 kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673033 --single -l 180 -s 20 SRR17673033.trim.fastq.gz
 kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673032 --single -l 180 -s 20 SRR17673032.trim.fastq.gz
 kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673031 --single -l 180 -s 20 SRR17673031.trim.fastq.gz
 kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673030 --single -l 180 -s 20 SRR17673030.trim.fastq.gz
+# individual leaves
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673027 --single -l 180 -s 20 SRR17673027.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673026 --single -l 180 -s 20 SRR17673026.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673025 --single -l 180 -s 20 SRR17673025.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673024 --single -l 180 -s 20 SRR17673024.trim.fastq.gz
 
 # summarize fastqc and kallisto mapping results in a single summary html using MultiQC
 # multiqc -d . 
