3b6a9f847f0d5b076711747e0a56f89b58aa33cf
vm-vh <vincentvonhaefen@gmail.com>
2023-04-15 17:05:35 +0200

    updated shell scrips to produce log files

--- kallisto.sh
+++ kallisto.sh
@@ -6,17 +6,14 @@
 kallisto index -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.fa
 
 # individual stems
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673033 --single -l 180 -s 20 SRR17673033.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673032 --single -l 180 -s 20 SRR17673032.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673031 --single -l 180 -s 20 SRR17673031.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673030 --single -l 180 -s 20 SRR17673030.trim.fastq.gz
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673033 --single -l 180 -s 20 SRR17673033.trim.fastq.gz &> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673032 --single -l 180 -s 20 SRR17673032.trim.fastq.gz &>> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673031 --single -l 180 -s 20 SRR17673031.trim.fastq.gz &>> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673030 --single -l 180 -s 20 SRR17673030.trim.fastq.gz &>> kallisto.log
 # individual leaves
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673027 --single -l 180 -s 20 SRR17673027.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673026 --single -l 180 -s 20 SRR17673026.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673025 --single -l 180 -s 20 SRR17673025.trim.fastq.gz
-kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673024 --single -l 180 -s 20 SRR17673024.trim.fastq.gz
-
-# summarize fastqc and kallisto mapping results in a single summary html using MultiQC
-# multiqc -d . 
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673027 --single -l 180 -s 20 SRR17673027.trim.fastq.gz &>> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673026 --single -l 180 -s 20 SRR17673026.trim.fastq.gz &>> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673025 --single -l 180 -s 20 SRR17673025.trim.fastq.gz &>> kallisto.log
+kallisto quant -i Coffea_canephora.AUK_PRJEB4211_v1.cdna.all.index -o k_SRR17673024 --single -l 180 -s 20 SRR17673024.trim.fastq.gz &>> kallisto.log
 
 echo "Finished"
--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,13 +1,16 @@
-# !/bin/bash
-
 # trimmomatic
 # needs to be run in the same folder as both the data and trimmomatic
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673024.fastq.gz SRR17673024.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673025.fastq.gz SRR17673025.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673026.fastq.gz SRR17673026.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673027.fastq.gz SRR17673027.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
 
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673030.fastq.gz SRR17673030.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673031.fastq.gz SRR17673031.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673032.fastq.gz SRR17673032.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
-java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673033.fastq.gz SRR17673033.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33
+# individual leaves
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673024.fastq.gz SRR17673024.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673025.fastq.gz SRR17673025.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673026.fastq.gz SRR17673026.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673027.fastq.gz SRR17673027.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+
+# individual stems
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673030.fastq.gz SRR17673030.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673031.fastq.gz SRR17673031.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673032.fastq.gz SRR17673032.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+java -jar ./Trimmomatic-0.39/trimmomatic-0.39.jar SE SRR17673033.fastq.gz SRR17673033.trim.fastq.gz ILLUMINACLIP:./Trimmomatic-0.39/adapters/TruSeq3-SE.fa:2:30:10 HEADCROP:5 SLIDINGWINDOW:4:15 MINLEN:70 -phred33 &>> trimmomatic.log
+
+echo "Finished"
